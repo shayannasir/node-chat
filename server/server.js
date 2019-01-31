02dd58f61dbeb0733a82ec90bcbace936fa8229a
shayannasir <sshayann7@gmail.com>
2019-01-31 23:12:57 +0530

    Added generateMessage() utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -15,34 +16,23 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit(
+    'newMessage',
+    generateMessage('Admin', 'Welcopme to the chat app')
+  );
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined',
-    createdAt: new Date().getTime()
-  });
+  // socket.broadcast = emits to all except the generator
+  socket.broadcast.emit(
+    'newMessage',
+    generateMessage('Admin', 'New User Joined')
+  );
 
   // socket.emit = Event generator, emits to a single person
   socket.on('createMessage', message => {
     console.log('Message Created', message);
 
     // io.emit = emits to every connection
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-    // socket.broadcast = emits to all except the generator
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   // socket.on = Event Listener
