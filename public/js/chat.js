6f5316d39137b3c7721aaa6f050f732ed51c7364
shayannasir <sshayann7@gmail.com>
2019-02-02 21:41:01 +0530

    Send messages to only people in the same room.

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,6 @@ jQuery('#message-form').on('submit', function(e) {
   socket.emit(
     'createMessage',
     {
-      from: 'User',
       text: messageTextbox.val()
     },
     function() {
--- server/server.js
+++ server/server.js
@@ -48,18 +48,25 @@ io.on('connection', socket => {
 
   // socket.emit = Event generator, emits to a single person
   socket.on('createMessage', (message, callback) => {
-    console.log('Message Created', message);
-
+    var user = users.getUser(socket.id);
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit(
+        'newMessage',
+        generateMessage(user.name, message.text)
+      );
+    }
     // io.emit = emits to every connection
-    io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
   });
 
   socket.on('createLocationMessage', coords => {
-    io.emit(
-      'newLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude)
-    );
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'newLocationMessage',
+        generateLocationMessage(user.name, coords.latitude, coords.longitude)
+      );
+    }
   });
 
   // socket.on = Event Listener
